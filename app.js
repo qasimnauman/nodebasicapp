d86a6f9f164d84dbfe071d7b50b95897b90f503c
Muhammad Qasim Nauman <123264838+qasimnauman@users.noreply.github.com>
2025-03-14 04:51:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello qasim deploy!');
 });
+
+app.get('/hello', function (req, res) {
+  res.send('Hello Route!');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
